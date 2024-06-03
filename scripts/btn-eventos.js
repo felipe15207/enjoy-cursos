bf4c61d1d21b953e9d71e5beebcf7cfda54fe708
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-03 00:17:08 -0300

    feed e rodape adcionados
    adicionando um feed com os cursos e o rodape do site

--- scripts/btn-eventos.js
+++ scripts/btn-eventos.js
@@ -1,4 +1,14 @@
+//codigo menu
 const logobtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 
 logobtn.addEventListener("click", () => menu.classList.toggle("active"));
+
+//codigo botao de login ou cadastro
+
+
+
+
+//codigo slider
+
+
