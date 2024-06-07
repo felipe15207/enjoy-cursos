f99e230dfcd2de8f94478880910532dd59695d36
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-07 13:05:56 -0300

    modificação das tag input
    mudança nos atributos das tags inputs como o tipo tel para o input de telefone e inputmode numeric

---
+++ formulario.php
@@ -0,0 +1,11 @@
+<?php
+    print_r($_POST['nome']);
+    print_r('<br>');
+    print_r($_POST['cpf']);
+    print_r('<br>');
+    print_r($_POST['telefone']);
+    print_r('<br>');
+    print_r($_POST['datanascimento']);
+    print_r('<br>');
+    print_r($_POST['senha']);
+    print_r('<br>');
