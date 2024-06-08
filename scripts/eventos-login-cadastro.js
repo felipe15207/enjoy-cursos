d3ea3c170569256acccd78ed8ee803f98f87df95
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-08 17:27:19 -0300

    mudanças drasticas nos formularios
    devido a falta de compatibilidade com modelos mobile do formulario anterior ele agora foi remodelado e dividido em 2 páginas uma para cadastro e outra para login

--- scripts/eventos-login-cadastro.js
+++ scripts/eventos-login-cadastro.js
@@ -1,16 +1,3 @@
-//codigo botao de login ou cadastro
-'use strict'
-
-        const loginContainer = document.getElementById('login-container')
-
-        const moveOverlay = () => loginContainer.classList.toggle('move')
-
-        document.getElementById('open-register').addEventListener('click', moveOverlay)
-        document.getElementById('open-login').addEventListener('click', moveOverlay)
-
-        document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
-        document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
-
 // Máscaras e restrições de caracteres
 document.getElementById('nome').addEventListener('input', function (event) {
     // Remove caracteres que não são letras ou espaços
@@ -21,3 +8,63 @@ document.getElementById('nome').addEventListener('input', function (event) {
 $('#datanascimento').mask('00/00/0000');
 $('#cpf').mask('000.000.000-00');
 $('#telefone').mask('(00) 00000-0000');
+
+//Validação do formulário
+$(document).ready(function() {
+    $("#formulario-cadastro").validate({
+      rules: {
+        nome: {
+          required: true,
+          minlength: 3
+        },
+        cpf: {
+          required: true,
+          minlength: 11
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        telefone: {
+          required: true,
+          minlength: 11
+        },
+        dataNascimento: {
+          required: true,
+          dateISO: true,
+          minlength: 8
+        },
+        senha: {
+          required: true,
+          minlength: 8
+        }
+      },
+      messages: {
+        nome: {
+          required: "Informe seu nome completo",
+          minlength: "O nome deve ter pelo menos 3 caracteres"
+        },
+        cpf: {
+          required: "Informe seu CPF",
+          digits: "O CPF deve conter apenas números",
+          minlength: "Informe seu CPF completo"
+        },
+        email: {
+          required: "Informe seu email",
+          email: "Informe um email válido"
+        },
+        telefone: {
+          required: "Informe seu telefone",
+          minlength: "Informe seu telefone completo"
+        },
+        datanascimento: {
+          required: "Informe sua data de nascimento",
+          dateISO: "Informe a data no formato DD/MM/AAAA"
+        },
+        senha: {
+          required: "Informe sua senha",
+          minlength: "A senha deve ter pelo menos 8 caracteres"
+        }
+      }
+    });
+  });
