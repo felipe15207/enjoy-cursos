2af2b26baf85645b5fa819f67f53657848af8891
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-07 23:47:36 -0300

    implemento tags input
    máscaras adicionadas com jquery

--- scripts/eventos-login-cadastro.js
+++ scripts/eventos-login-cadastro.js
@@ -17,3 +17,7 @@ document.getElementById('nome').addEventListener('input', function (event) {
     this.value = this.value.replace(/[^a-zA-ZÀ-ÿ\s]/g, '');
     }
 );
+// Máscaras usando o maskplugin
+$('#datanascimento').mask('00/00/0000');
+$('#cpf').mask('000.000.000-00');
+$('#telefone').mask('(00) 00000-0000');
