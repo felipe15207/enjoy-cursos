d2ed93aa6b15209ca140f3b6537308bb8c65f031
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-09 18:36:38 -0300

    barra de pesquisa adicionada

---
+++ scripts/sugestoes-home.js
@@ -0,0 +1,10 @@
+let suggestions = [
+    "Introdução à Álgebra", "Gramática Avançada", 
+    "Programação em Python", "Química Orgânica", 
+    "Cálculo Diferencial e Integral", "Literatura Brasileira", 
+    "Desenvolvimento Web", "Química Inorgânica", "Estatística Aplicada", 
+    "Redação Técnica", "Inteligência Artificial", "Bioquímica", 
+    "Geometria Analítica", "Interpretação de Textos", "Banco de Dados", 
+    "Química Analítica", "Teoria dos Números", "Produção Textual", 
+    "Engenharia de Software", "Físico-Química"
+];
