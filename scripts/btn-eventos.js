479dfb70a04c88106e9f7dfea0160fd9fd34082f
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-09 21:57:08 -0300

    botão de login ou cadastro
    Agora quando o botão de login é presionado uma caixa com as opções de cadastro ou login aparece

--- scripts/btn-eventos.js
+++ scripts/btn-eventos.js
@@ -3,3 +3,9 @@ const logobtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 
 logobtn.addEventListener("click", () => menu.classList.toggle("active"));
+
+$(document).ready(function(){
+    $(".user-container").click(function(){
+        $(".caixa-login-cadastro").toggle(); // Use toggle() em vez de show()
+    });
+});
